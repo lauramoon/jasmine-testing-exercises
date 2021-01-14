4af354d0812b21b9c1ed7ecb35bc5da0fac0d990
Laura Moon <laura.kolb@gmail.com>
2021-01-14 12:55:54 -0600

    complete calculator and tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,36 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it('should calculate the monthly payment correctly', function () {
+  expect(calculateMonthlyPayment(testValues1)).toEqual('106.07');
+  expect(calculateMonthlyPayment(testValues2)).toEqual('321.75');
 });
-
-
 it("should return a result with 2 decimal places", function() {
-  // ..
+  expect(calculateMonthlyPayment(testValues1).slice(-3,-2)).toEqual('.');
+  expect(calculateMonthlyPayment(testValues2).slice(-3,-2)).toEqual('.');
 });
+it('should handle loan amount of 0', () => {
+  testZeroLoan = {amount: 0, years: 5, rate: 0.5}
+  expect(calculateMonthlyPayment(testZeroLoan)).toEqual('0.00');
+})
+it('should handle negative loan amount', () => {
+  testNegativeLoan = {amount: -1000, years: 5, rate: 0.5}
+  expect(calculateMonthlyPayment(testNegativeLoan)).toEqual('The loan amount cannot be negative');
+})
+it('should handle rate of 0', () => {
+  testZeroRate = {amount: 12000, years: 5, rate: 0}
+  expect(calculateMonthlyPayment(testZeroRate)).toEqual('200')
+})
+it('should handle negative rate', () => {
+  testNegativeRate = {amount: 10000, years: 5, rate: -.03}
+  expect(calculateMonthlyPayment(testNegativeRate)).toEqual('The interest rate must be positive')
+})
+it('should check for unexpectedly large interest rate', () => {
+  testLargeInterest = {amount: 10000, years: 5, rate: 5}
+  expect(calculateMonthlyPayment(testLargeInterest))
+    .toEqual('Please enter the interest rate as a decimal (for example, 5% as 0.05)')
+})
 
-/// etc
+
+beforeAll(() => {
+  testValues1 = {amount: 10000, years: 10, rate: .05};
+  testValues2 = {amount: 50000, years: 15, rate: .02};
+})
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,49 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").value = 10000;
+  document.getElementById("loan-years").value = 10;
+  document.getElementById("loan-rate").value = 0.05;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let values = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(values));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let numPayments = values.years*12;
+  let decimal = 0;
+  let monthlyRate = values.rate/12;
+  let principle = values.amount;
+
+  if (numPayments < 12) {
+    return "The term must be at least one year"
+  } else if (principle === 0) {
+    return '0.00'
+  } else if (principle < 0) {
+    return 'The loan amount cannot be negative'
+  } else if (monthlyRate === 0) {
+    decimal = principle/numPayments;
+  } else if (monthlyRate < 0) {
+    return 'The interest rate must be positive'
+  } else if (values.rate > 1) {
+    return 'Please enter the interest rate as a decimal (for example, 5% as 0.05)'
+  } else {
+    decimal = principle*monthlyRate/(1-(1+monthlyRate)**(-numPayments));
+  }
+  
+  let rounded = Math.round(decimal*100)/100;
+  return rounded.toString();
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.getElementById('monthly-payment').textContent = monthly;
 }
